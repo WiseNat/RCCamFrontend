2fec9ffe8d9e45d95e02b22772c0c7a8084f13f4
WiseNat <nathan88wise@gmail.com>
2020-08-14 14:02:57 +0100

    Font fully implemented

--- app/src/main/java/com/example/rccamfrontend/MainActivity.kt
+++ app/src/main/java/com/example/rccamfrontend/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.example.rccamfrontend
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Typeface
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val typeface = Typeface.createFromAsset(assets, "fonts/agencyb.ttf")
+
+        textfieldIP.typeface = typeface
+        textfieldPort.typeface = typeface
+        btnConnect.typeface = typeface
+
     }
 }
