c3e5c05588558d29d08fed76665d1122f2e5ffeb
WiseNat <nathan88wise@gmail.com>
2020-08-17 17:34:31 +0100

    Basic Webview implementation

--- app/src/main/java/com/example/rccamfrontend/MainActivity.kt
+++ app/src/main/java/com/example/rccamfrontend/MainActivity.kt
@@ -1,13 +1,21 @@
 package com.example.rccamfrontend
 
-import android.graphics.Typeface
 import android.os.Bundle
+import android.webkit.WebView
 import androidx.appcompat.app.AppCompatActivity
-import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        if (intent != null) {
+            val webview = findViewById<WebView>(R.id.webView)
+            webview.loadUrl("http://%s:%s/".format(
+                intent.getStringExtra("ip"),
+                intent.getStringExtra("port")))
+        }
+
+
     }
 }
