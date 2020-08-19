bc54eab9a96db99391fd1b27c39bed8b56918598
WiseNat <nathan88wise@gmail.com>
2020-08-19 11:53:39 +0100

    Webview Error Checking

--- app/src/main/java/com/example/rccamfrontend/ConnectActivity.kt
+++ app/src/main/java/com/example/rccamfrontend/ConnectActivity.kt
@@ -2,6 +2,10 @@ package com.example.rccamfrontend
 
 import android.content.Intent
 import android.os.Bundle
+import android.webkit.WebResourceError
+import android.webkit.WebResourceRequest
+import android.webkit.WebView
+import android.webkit.WebViewClient
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_connect.*
@@ -21,36 +25,50 @@ class ConnectActivity : AppCompatActivity() {
             var nextActivity = true
 
             if(ipInput == ""){ // Checking if a value was entered for the IP
-                generateToast("Enter an IP Address")
+                generateToast(this, "Enter an IP Address")
                 nextActivity = false
 
             }else if (!ipResult) { // Checking if IP address is valid
-                generateToast("Invalid IP Address")
+                generateToast( this,"Invalid IP Address")
                 nextActivity = false
             }
 
             if(portInput == null){ // Checking if a value was entered for the port
-                generateToast("Enter a Port number")
+                generateToast(this, "Enter a Port number")
                 nextActivity = false
 
             } else if(portInput > 65536){ // Checking if port number is valid
-                generateToast("Invalid Port")
+                generateToast(this, "Invalid Port")
                 nextActivity = false
             }
 
-            if (nextActivity){
-                // Start Main activity
-                val intent = Intent(this, MainActivity::class.java)
+            if (nextActivity){ // Process for Main activity
 
-                intent.putExtra("ip", textfieldIP.text.toString())
-                intent.putExtra("port", textfieldPort.text.toString())
-                startActivity(intent)
+                var webviewError = false
+                val hiddenWebview = findViewById<WebView>(R.id.hiddenWebview)
+                hiddenWebview.webViewClient = object : WebViewClient() { // Webpage failed to load
+                    override fun onReceivedError (view: WebView, request: WebResourceRequest, error: WebResourceError) {
+                        generateToast(this@ConnectActivity, error.description.toString(), dur=Toast.LENGTH_LONG)
+                        webviewError = true
+                    }
+
+                    override fun onPageFinished(view: WebView?, url: String?) { // Go to next activity
+                        super.onPageFinished(view, url)
+
+                        if (!webviewError){
+                            val intent = Intent(this@ConnectActivity, MainActivity::class.java)
+                            intent.putExtra("ip", textfieldIP.text.toString())
+                            intent.putExtra("port", textfieldPort.text.toString())
+
+                            startActivity(intent)
+                        }
+                    }
+                }
+
+                hiddenWebview.loadUrl("http://%s:%s/".format(
+                    textfieldIP.text.toString(),
+                    textfieldPort.text.toString()))
             }
         }
     }
-
-    private fun generateToast(text: String, duration: Int = Toast.LENGTH_SHORT){
-        Toast.makeText(this, text, duration).show()
-    }
-
 }
