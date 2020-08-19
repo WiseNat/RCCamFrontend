c4c94af4fd0d239eb614d8cba8ac09447db12b71
WiseNat <nathan88wise@gmail.com>
2020-08-19 12:28:02 +0100

    Basic Main GUI - it looks really nasty

--- app/src/main/java/com/example/rccamfrontend/MainActivity.kt
+++ app/src/main/java/com/example/rccamfrontend/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.example.rccamfrontend
 
-import android.content.Intent
 import android.os.Bundle
 import android.webkit.WebResourceError
 import android.webkit.WebResourceRequest
@@ -8,7 +7,6 @@ import android.webkit.WebView
 import android.webkit.WebViewClient
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
